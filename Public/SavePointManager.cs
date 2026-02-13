f16aed5f01463ac985f9c74777d3f3671e93fc05
dashino0016-svg <dashino0016@gmail.com>
2026-02-14 00:48:25 +0800

    Revert "Reset enemies to home during respawn black-screen"

--- Public/SavePointManager.cs
+++ Public/SavePointManager.cs
@@ -355,20 +355,7 @@ public class SavePointManager : MonoBehaviour
 
     void RefreshEnemiesDuringBlackScreen()
     {
-        EnemyController[] enemies = FindObjectsByType<EnemyController>(FindObjectsSortMode.None);
-        if (enemies == null || enemies.Length == 0)
-            return;
-
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            EnemyController enemy = enemies[i];
-            if (enemy == null)
-                continue;
-
-            LostTarget lostTarget = enemy.GetComponent<LostTarget>();
-            Transform homePoint = lostTarget != null ? lostTarget.homePoint : null;
-            enemy.ResetToHomeForCheckpoint(homePoint);
-        }
+        // TODO(step6): Respawn enemies to their HomePoint (NotCombat state).
     }
 
     void AlignPlayerToAnchor(Transform anchor)
